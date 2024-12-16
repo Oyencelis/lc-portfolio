1ea56ed54448aae352a2ad344cb21f6f32218c6c
Oyencelis <oyencelis@gmail.com>
2024-12-17 07:41:19 +0800

    Move sitemap to a public

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteSitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteSitemap({
-      hostname: 'https://oyencelis.vercel.app', // Replace with your actual domain
-    }),
-  ],
+  base: './',
+  plugins: [react()],
+  build: {
+  },
 })
